06c340c40ecf6eb7ee14b51fd51120d80f6d31ba
Maicon Paes Bez Birolo <maicondante@hotmail.com>
2025-12-04 01:50:56 -0300

    Adiciona a página inicial

---
+++ pages/index.js
@@ -0,0 +1,28 @@
+function home() {
+    return (
+    <div style={{
+        border: "2px solid pink",
+        padding: "20px",
+        width: "800px",
+        margin: "auto",
+        borderRadius: "8px",
+      }}>
+    <h1 style={{
+        border: "2px solid red",
+        padding: "16px",
+        borderRadius: "8px" }}>
+    <strong>Nunca se esqueça de quanto tu és ESPECIAL BB</strong>
+    </h1>
+    <h2>
+        <em>Andréa Pénelope Charmosa</em>
+    </h2>
+    <img style={{
+        width: "800px",
+        height: "400px"
+    }} 
+    src="https://conteudo.imguol.com.br/c/entretenimento/2c/2020/12/14/amor-relacionamento-casal-paixao-apaixonados-1607980291163_v2_1x1.jpg" alt="Foto Mão Coração"/>
+    </div>
+    )
+}
+
+export default home;
